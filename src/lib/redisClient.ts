51a73355c8021bf1c56725d2a7f3f60f3df454a1
Baraka mukelenga <baraka.m@code-care.pro>
2024-12-14 14:55:06 +0200

    Update Redis client configuration to use environment variables for host and password

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -1,10 +1,23 @@
-import { connect } from "https://deno.land/x/redis/mod.ts";
+import { load } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
+import { createClient } from "npm:redis@4.6.11";
 
-const redis = await connect({
-  hostname: "127.0.0.1",
-  port: 6379,
+await load({ export: true });
+
+const REDIS_HOST = Deno.env.get("REDIS_HOST") || "127.0.0.1";
+const REDIS_PASSWORD = Deno.env.get("REDIS_PASSWORD") || null;
+
+const client = createClient({
+  username: "default",
+  password: REDIS_PASSWORD!,
+  socket: {
+    host: REDIS_HOST,
+    port: 10806,
+  },
 });
 
-console.log("Connected to Redis");
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+const redis = await client.connect();
+console.log("Connected to Redis!");
 
 export default redis;
